eec9d61d7bec5382458a4f27c173fd675997266d
Urwa6 <115958392+Urwa6@users.noreply.github.com>
2025-10-23 03:30:40 -0900

    Created class Player
    Players handles player input(which cells to reveal)

--- src/se/team/minesweeper/Player.java
+++ src/se/team/minesweeper/Player.java
@@ -1,4 +1,16 @@
-package se.team.minesweeper;
-
+import java.util.Scanner;
+//Player handles player input(which cell to reveal)
 public class Player {
+    private final Scanner scanner = new Scanner(System.in);
+
+    //Asking the player after row & column and returns them as an array
+    public int[] makeMove() {
+        System.out.print("Ange rad och kolumn (t.ex. '2 3'): ");
+        //Row
+        int r = scanner.nextInt();
+        //Column
+        int c = scanner.nextInt();
+        return new int[]{r, c};
+    }
 }
+
