b967df44fb2fdd2d2e3e8458595372b21574b550
Urwa6 <115958392+Urwa6@users.noreply.github.com>
2025-10-23 03:21:45 -0900

    Created Main for starting the Game

--- src/se/team/minesweeper/Main.java
+++ src/se/team/minesweeper/Main.java
@@ -1,8 +1,10 @@
-package se.team.minesweeper;
-
+//Creating a new Game-object & starts the game
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.startGame();
+        //Creating a new game : 8x8-board with 10 mines
+        Game game = new Game(8, 8, 10);
+
+        //Start the game
+        game.start();
     }
 }
